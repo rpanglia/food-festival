64338052d7f2811f8feab670148f67b671efb320
Ravneet Panglia <ravneet.panglia@gmail.com>
2022-01-21 15:57:05 -0500

    loads of changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,30 @@
 const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
-
 const path = require("path");
 const webpack = require("webpack");
 
+
 module.exports = {
-    entry: './assets/js/script.js',
+    entry: {
+        app: "./assets/js/script.js",
+        events: "./assets/js/events.js",
+        schedule: "./assets/js/schedule.js",
+        tickets: "./assets/js/tickets.js"
+    },
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.bundle.js'
+        filename: "[name].bundle.js",
+        path: __dirname + "/dist",
+    },
+    module: {
+        rules: [
+            {
+              test: /\.jpg$/i,
+              use: [
+                {
+                  loader: 'file-loader'
+                }
+              ]
+            }
+        ]
     },
     plugins: [
         new webpack.ProvidePlugin({
